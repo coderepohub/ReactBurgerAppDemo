098f5405d5b7e1f7483a9686a8840673a4f0f4a8
h346363 <Aman.Kumar@Honeywell.com>
2019-06-06 17:52:01 +0530

    SideDrawer Implimented... (without dynamic controll)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Aux from '../../hoc/AuxControl';
 import classes from '../Layout/Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
+        <SideDrawer/>
        <Toolbar/>
         <main className={classes.Container}>
             {props.children}
