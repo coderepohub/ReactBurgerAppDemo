acdf4c036ebe294c6002b38019489e6ae66f0047
h346363 <Aman.Kumar@Honeywell.com>
2019-06-07 14:18:44 +0530

    SideDrawer and hamburgermenu added - basic development for an app is done..

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/AuxControl';
 import classes from '../Layout/Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <SideDrawer/>
-       <Toolbar/>
-        <main className={classes.Container}>
-            {props.children}
-            
-        </main>
-    </Aux>
+class Layout extends Component{ 
+    
+    state={
+        showSideDrawer:false
+    }
+    sideDrawerCloseHandler=()=>{
+        this.setState({showSideDrawer:false});
+    }
+    sideDrawerToggleHandler=()=>{
+        this.setState((prevState)=>{
+            return{showSideDrawer:!prevState.showSideDrawer}
+        });
+    }
+ 
 
-);
+    render(){
+        return(  
+        <Aux>
+           <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+            <SideDrawer closed={this.sideDrawerCloseHandler} open={this.state.showSideDrawer}/>
+            <main className={classes.Container}>
+                {this.props.children}
+                
+            </main>
+        </Aux>
+    
+    );
+    
+    }
+  
+}
 
-export default layout;
+export default Layout;
 
